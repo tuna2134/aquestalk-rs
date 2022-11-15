c5ec36b5e58e2065c9af72be7a4b20dd4c3af348
mc_fdc <mc-fdc@tuna2134.jp>
2022-11-15 21:16:28 +0900

    add: okay

---
+++ src/aquestalk.rs
@@ -0,0 +1,45 @@
+use libc::{c_char, c_int, c_uchar, c_void};
+use std::{error, ffi::CString, fmt, slice};
+
+pub type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[link(name = "AquesTalk2Eva", kind = "dylib")]
+extern "C" {
+    fn AquesTalk_Synthe_Utf8(
+        koe: *const c_char,
+        iSpeed: c_int,
+        size: *const c_int,
+    ) -> *const c_uchar;
+
+    fn AquesTalk_FreeWave(wav: *const c_uchar) -> c_void;
+}
+
+#[derive(Debug)]
+pub enum AquesTalkError {
+    Error(i32),
+}
+
+impl fmt::Display for AquesTalkError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AquesTalkError::Error(e) => write!(f, "AquesTalkError: {}", e),
+        }
+    }
+}
+
+impl std::error::Error for AquesTalkError {}
+
+pub fn synthe(text: &str, speed: i32) -> Result<Vec<u8>> {
+    let size: i32 = 0;
+    let content: CString = CString::new(text).unwrap();
+    let phont: *const c_void = std::ptr::null();
+    let wav: *const u8 =
+        unsafe { AquesTalk_Synthe_Utf8(content.as_ptr(), speed, &size as *const i32, phont) };
+    if wav.is_null() {
+        return Err(Box::new(AquesTalkError::Error(size)));
+    }
+    unsafe {
+        AquesTalk_FreeWave(wav);
+    };
+    return unsafe { Ok(slice::from_raw_parts(wav, size.try_into().unwrap()).to_vec()) };
+}
