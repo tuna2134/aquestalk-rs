0b805997769450a949984a2d068eeb6aba5408f1
mc_fdc <mc-fdc@tuna2134.jp>
2022-11-16 16:11:25 +0900

    Update basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,5 +4,5 @@ use std::fs;
 
 fn main() {
     let wav = aquestalk::synthe("これはてすとです", 100).unwrap();
-    fs::write("test.wav", wav.to_vec()).unwrap();
+    fs::write("basic.wav", wav.to_vec()).unwrap();
 }
