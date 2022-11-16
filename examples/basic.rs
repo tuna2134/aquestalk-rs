39695cab9be3b40074836f13c60b1eb4e2ce59fe
mc_fdc <mc-fdc@tuna2134.jp>
2022-11-16 16:08:40 +0900

    Update basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,6 +3,6 @@ extern crate aquestalk;
 use std::fs;
 
 fn main() {
-    let wav = aquestalk::synthe("こんにちは", 100).unwrap();
+    let wav = aquestalk::synthe("こんにちは　せかい", 100).unwrap();
     fs::write("test.wav", wav.to_vec()).unwrap();
 }
